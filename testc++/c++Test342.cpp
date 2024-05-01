d26ab9972ff7e86b9706cfd707e0ec2146798ae2
Awith <676728621@qq.com>
2024-05-01 19:18:56 +0800

    2024.05.01

--- testc++/c++Test342.cpp
+++ testc++/c++Test342.cpp
@@ -21,39 +21,10 @@ typedef pair<ll,ll> pll;
 #define endl "\n"
 #define ios {ios::sync_with_stdio(0);cin.tie(0);}
 const int N=2e5+10;
-ll n, k;
-ll a[N];
-ll res;
-bool check(ll mid){
-    ll y = k;
-    for(int i=0;i<n;i++){
-		if(a[i]<mid) y-=(mid-a[i]);
-	}
-    if(y>=0) res = y;
-    return y >= 0 ? true : false;
-}
 
 void Solve()
 {
-    cin >> n >> k;
-    for (int i = 0; i < n;i++) cin >> a[i];
-    sort(a, a + n);
-    ll ans = 0;
-    ll l = 0, r = a[n - 1] + k;
-    while(l+1<r){
-        ll mid = (l + r) / 2;
-        if (check(mid)){
-            l = mid;
-        }else{
-            r = mid;
-        }
-    }
-    ll cnt = 0;
-    for (int i = 0; i < n;i++){ 
-        if(a[i]<=l) cnt++;
-    }
-    ans = n * l - (cnt - 1 - res);
-    cout << ans << endl;
+    
 }
 
 int main()
