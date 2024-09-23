7fd6213f0bdc9388ae0df54cd319d3fdcc6fa7d4
qian488 <3517576561@qq.com>
2024-09-23 19:00:32 +0800

    complete CS50 week5

--- CS50_Start/CS50x2023_MySolution/Week5-Data_Structures/Lab5/inheritance.c
+++ CS50_Start/CS50x2023_MySolution/Week5-Data_Structures/Lab5/inheritance.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+// 建树
+
 // Each person has two parents and two alleles
 typedef struct person
 {
@@ -40,6 +42,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *new_person = (person*)malloc(sizeof(person*));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -49,8 +52,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        new_person->alleles[0] = parent0->alleles[rand()%2];
+        new_person->alleles[1] = parent1->alleles[rand()%2];
 
     }
 
@@ -58,23 +65,33 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
         // TODO: Randomly assign alleles
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
 
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
+    
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
