dd5ccc6caca87aa408e9f77a9a093dd60f7610b7
qian488 <3517576561@qq.com>
2024-10-06 22:38:20 +0800

    completed cs61a hw02

--- CS61A/CS61A-2024-MySolution/Homework/hw02/hw02.py
+++ CS61A/CS61A-2024-MySolution/Homework/hw02/hw02.py
@@ -32,6 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    ans = 1
+    for i in range(1,n + 1):
+        ans *= term(i)
+    return ans
 
 
 def accumulate(fuse, start, n, term):
@@ -54,6 +58,10 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
+    ans = start
+    for i in range(1,n+1):
+        ans = fuse(ans,term(i))
+    return ans
 
 
 def summation_using_accumulate(n, term):
@@ -68,7 +76,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add,0,n,term)
 
 
 def product_using_accumulate(n, term):
@@ -83,7 +91,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul,1,n,term)
 
 
 def make_repeater(f, n):
@@ -100,4 +108,11 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    def repeater(x):
+        ans = x
+        for _ in range(n):
+            ans = f(ans)
+        return ans
+    
+    return repeater
 
